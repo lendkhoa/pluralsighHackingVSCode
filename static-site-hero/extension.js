8556b74c7331d6f67baaf3068939a1f05d05bd35
KL052362 <khoa.le@cerner.com>
2017-10-10 14:49:10 -0500

    static-site-hero: create the functions to update date

--- static-site-hero/extension.js
+++ static-site-hero/extension.js
@@ -1,26 +1,67 @@
+'use strict'
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 var vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+let insertText = (value) => {
+    let editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        vscode.window.showErrorMessage("Can't insert text because no document is open");
+        return;
+    }
+    
+    let selection = editor.selection;
+    let range = new vscode.Range(selection.start, selection.end);
+
+    editor.edit((editBuilder)=>{
+        editBuilder.replace(range, value);
+    });
+
+};
+
+let getImageTemplate =()=>{
+    return vscode.workspace.getConfiguration("staticSiteHero")["imagePathTemplate"];
+}
+
+let getFileTemplate =()=>{
+    return vscode.workspace.getConfiguration("staticSiteHero")["filePathTemplate"];
+}
+
+let updateTemplateWithDate=(template)=>{
+    let today = new Date();
+    let year = today.getFullYear();
+    // slice the result, take the last two digits
+    let month = ('0' + (today.getMonth() + 1)).slice(-2);
+    template = template.replace("${year}", year);
+    template = template.replace("${month}", month);
+    return template;
+};
+
 function activate(context) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "static-site-hero" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    var disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+    let fileLinkDisposable = vscode.commands.registerCommand("extension.insertLink", ()=>{
+        let linkTypeList = ['File', 'Image'];
+        vscode.window.showQuickPick(linkTypeList, {placeHolder: "Link type"}).then(
+            result=>{
+                if (result === "File") {
+                    insertText(updateTemplateWithDate(getFileTemplate()));
+                } else {
+                    insertText(updateTemplateWithDate(getImageTemplate()));
+                }
+            }
+        );
     });
+    context.subscriptions.push(fileLinkDisposable);
+    
+    let figureDisposable = vscode.commands.registerCommand("extension.insertFigure", ()=>{
+        vscode.window.showInformationMessage('selected insert figure');
+    });
+    context.subscriptions.push(figureDisposable);
 
-    context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
